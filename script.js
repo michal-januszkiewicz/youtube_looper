c756b5038373ef21825ccfaac04209664ffdb2ee
michal <mljc92@gmail.com>
2016-06-03 14:49:55 +0200

    Add video parameters and loop the playing

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ $(document).ready(function() {
   $(".search").click(function() {
     var coreLink = "https://www.youtube.com/embed/"
     var userLink = $(".video-link").val();
+    var videoID = "";
     var link = "";
+    var params = "?autohide=1&iv_load_policy=3&loop=1&rel=0&playlist=";
 
-    link = coreLink.concat(userLink);
-    if (userLink.search("watch") > 0) {
+    var result = userLink.search("watch");    
+    if (result > 0) {
+      var index = result + "watch?v=".length;
+      videoID = userLink.substring(index);
       link = userLink.replace("watch?v=", "embed/");
     }
+    else {
+      videoID = userLink;
+      link = coreLink.concat(userLink);
+    }
+
+    link = link.concat(params, videoID);
     $(".video").attr("src", link);
   });
 });
