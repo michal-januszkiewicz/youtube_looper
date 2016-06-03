5e6dd4067ea0f77affd8e510e7070d0e86d85f0a
michal <mljc92@gmail.com>
2016-06-03 14:03:04 +0200

    Load a youtube video from the form

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".play").click(function() {
+    var coreLink = "https://www.youtube.com/embed/"
+    var userLink = $(".video-link").val();
+    var link = "";
+
+    link = coreLink.concat(userLink);
+    if (userLink.search("watch") > 0) {
+      link = userLink.replace("watch?v=", "embed/");
+    }
+    $(".video").attr("src", link);
+  });
+});
