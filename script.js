86bf6be402b2848e3c7bd8e02e32b3ce54f44e2c
michal <mljc92@gmail.com>
2016-06-05 18:46:04 +0200

    Add comments to explain how the program works

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
+
+  // User clicked 'Search' button.
   $(".search").click(function() {
+
+    // Hide error message.
     $(".error").hide();
+
+    // Get the link from the input.
     var userLink = $(".video-link").val();
+
+    // Extract video id from the link.
     var videoID = getVideoID(userLink);
+
+    // Check if link is valid and the video exists.
     result = checkLink(videoID);
+
+    // If link is invalid.
     if (result === false) {
+      // Show error message and hide the video.
       $(".error").show();
       $(".video").hide();
     }
+
+    // If link is valid.
     else {
+      // Create a loopable link.
       var coreLink = "https://www.youtube.com/embed/"
       var params = {
         autohide       : 1,
@@ -18,11 +34,15 @@ $(document).ready(function() {
         playlist       : videoID,
       }
       var link = prepareLink(coreLink, videoID, params);
+
+      // And display it.
       $(".video").attr("src", link).show();
     }
   });
 });
 
+
+// Prepare a link with a video.
 function prepareLink(coreLink, videoID, params) {
   var link = coreLink.concat(videoID);
   params = stringifyParams(params);
@@ -30,6 +50,8 @@ function prepareLink(coreLink, videoID, params) {
   return link;
 }
 
+
+// Convert params dictionary to a string.
 function stringifyParams(params) {
   var stringifiedParams = "?";
   for (var param in params) {
@@ -40,7 +62,9 @@ function stringifyParams(params) {
   return stringifiedParams;
 }
 
+// Extract video id from the link.
 function getVideoID(userLink) {
+    // If user entered just the id do nothing.
     var videoID = userLink;
     var result = userLink.search("watch");    
     if (result > 0) {
@@ -50,6 +74,8 @@ function getVideoID(userLink) {
     return videoID;
 }
 
+// Check if video id is valid and the video exists  
+// using youtube API.
 function checkLink(videoID) {
   var coreLink = "https://www.googleapis.com/youtube/v3/videos";
   var params = {
@@ -59,7 +85,10 @@ function checkLink(videoID) {
   }
   params = stringifyParams(params);
 
+  // Prepare a link for API.
   var link = coreLink.concat(params);
+
+  // Check youtube API.
   var result = false;
   $.ajax({
     type: "GET",
